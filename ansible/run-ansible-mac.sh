0d577b48971383f0764ddc3918724bb8276e34aa
dalisoft <dalisoft@mail.ru>
2020-05-19 17:35:20 +0500

    add modules

--- ansible/run-ansible-mac.sh
+++ ansible/run-ansible-mac.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd ansible.mac
 ansible-galaxy install -r requirements.yml
-ansible-playbook --connection=local --inventory 127.0.0.1, site.yml
+ansible-playbook -v --connection=local --inventory 127.0.0.1, site.yml
