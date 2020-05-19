17dd4d9542a1cae085a6a4fbf60dfe539321d63a
dalisoft <dalisoft@mail.ru>
2020-05-19 21:36:53 +0500

    update ansible config

--- ansible/ansible.mac/runme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-[ -z $(which brew) ] && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-[ -z $(which ansible) ] && brew install ansible
-[ -z $(which fish) ] && brew install fish
-
-exit 0
