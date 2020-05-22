613865f9ae03d200a5dd7349fc1008d9ffc42b54
dalisoft <dalisoft@mail.ru>
2020-05-23 00:30:40 +0500

    Link instead of copy

--- run-dotfiles.sh
+++ run-dotfiles.sh
@@ -3,8 +3,9 @@
 echo "Backup up currently mackup config"
 (mv ~/.mackup.cfg ~/.mackup.cfg.bak) || echo "Currently mackup config does not exist"
 
-echo "Copy current directory mackup config to home directory"
-cp dotfiles/.mackup.cfg ~/.mackup.cfg
+echo "Link necessary configs to home directory"
+ln -s $(pwd)/dotfiles/.mackup.cfg ~/.mackup.cfg
+ln -s $(pwd)/dotfiles/.gitconfig ~/.gitconfig
 
 echo "Restore from mackup"
 mackup restore
