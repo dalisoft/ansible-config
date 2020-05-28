de07cc5c013e1d3c2899fd4aca7af7cbb97260f7
dalisoft <dalisoft@mail.ru>
2020-05-28 10:31:32 +0500

    clean repo

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,10 +4,20 @@
 
 set -gx fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 set -gx fish_user_paths "$HOME/.npm-global/bin" $fish_user_paths
+set -gx fish_user_paths "$HOME/go/bin" $fish_user_paths
+set -gx fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
+set -gx fish_user_paths "$HOME/lsp/bin" $fish_user_paths
 
 ###################################
 ######### env parameters  #########
 ###################################
+
+switch (uname)
+  case Darwin
+    # set -gx DOCKER_HOST "ssh://root@$REMOTE_MACHINE_IP"
+  case Linux
+    # echo "Linux"
+end
 
 # **** Brew ****
 set -gx HOMEBREW_NO_ANALYTICS 1
@@ -19,7 +29,14 @@ set -gx ANDROID_HOME "$HOME/Library/Android/sdk"
 set -gx NPM_CONFIG_PREFIX "$HOME/.npm-global"
 
 # **** BAT ****
-set -gx BAT_THEME "GitHub"
+set -gx BAT_THEME "Monokai Extended Bright"
+
+# **** GO ****
+set -gx GOPATH "$HOME/go"
+
+# **** Rust (Cargo) ****
+set -gx CARGO_INSTALL_ROOT "$HOME/.cargo"
+set -gx CARGO_HOME "$HOME/.cargo"
 
 # ********************************
 # ********* openssl env **********
@@ -42,6 +59,15 @@ set -gx PATH "$ANDROID_HOME/platform-tools" $PATH
 # **** NPM ****
 set -gx PATH "$NPM_CONFIG_PREFIX/bin" $PATH
 
+# **** GO ****
+set -gx PATH "$HOME/go/bin" $PATH
+
+# **** Rust (Cargo) ****
+set -gx PATH "$HOME/.cargo/bin" $PATH
+
+# **** LSP ****
+set -gx PATH "$HOME/lsp/bin" $PATH
+
 ###################################
 ############# aliases #############
 ###################################
@@ -50,6 +76,7 @@ set -gx PATH "$NPM_CONFIG_PREFIX/bin" $PATH
 alias cat=bat
 alias ls=exa
 alias grep=rg
+alias vim=nvim
 
 # **** git clean alias ****
 alias git_reflog_expire="git reflog expire --expire=now --all"
@@ -64,6 +91,9 @@ alias venv "source $HOME/env/bin/activate.fish" # Activate V-Env by request
 
 # **** fnm ****
 alias load_fnm "fnm env --shell=fish --multi | source"
+
+# **** test internet speed ****
+alias inet_speed_test "curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -"
 
 ###################################
 ########## init scripts ###########
