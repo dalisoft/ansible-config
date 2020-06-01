dd19f43e3056de36f1c711bf4878e12ad9d7a29b
dalisoft <dalisoft@mail.ru>
2020-06-01 18:06:15 +0500

    fix(config): linux homebrew shell add

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,8 @@ set -gx fish_user_paths "$HOME/.npm-global/bin" $fish_user_paths
 set -gx fish_user_paths "$HOME/go/bin" $fish_user_paths
 set -gx fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
 set -gx fish_user_paths "$HOME/lsp/bin" $fish_user_paths
+set -gx fish_user_paths "/home/linuxbrew/.linuxbrew/bin" $fish_user_paths
+set -gx fish_user_paths "/home/linuxbrew/.linuxbrew/sbin" $fish_user_paths
 
 ###################################
 ######### env parameters  #########
@@ -17,6 +19,13 @@ switch (uname)
     # set -gx DOCKER_HOST "ssh://root@$REMOTE_MACHINE_IP"
   case Linux
     # echo "Linux"
+    set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+    set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+    set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+
+    set -gx MANPATH "$HOMEBREW_PREFIX/share/man" $MANPATH;
+    set -gx INFOPATH "$HOMEBREW_PREFIX/share/info" $INFOPATH
+
 end
 
 # **** Brew ****
@@ -55,6 +64,10 @@ set -gx PATH "$ANDROID_HOME/emulator" $PATH
 set -gx PATH "$ANDROID_HOME/tools" $PATH
 set -gx PATH "$ANDROID_HOME/tools/bin" $PATH
 set -gx PATH "$ANDROID_HOME/platform-tools" $PATH
+
+# **** Homebrew - Linux ****
+set -gx PATH "$HOMEBREW_PREFIX/bin" $PATH
+set -gx PATH "$HOMEBREW_PREFIX/sbin" $PATH
 
 # **** NPM ****
 set -gx PATH "$NPM_CONFIG_PREFIX/bin" $PATH
