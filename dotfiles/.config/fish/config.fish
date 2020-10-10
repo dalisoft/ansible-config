0052387431a9c2e0c01bec961967ddb37ebb44e0
dalisoft <dalisoft@mail.ru>
2020-10-11 01:47:02 +0500

    fix: this may fix python matplotlib bug

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -52,6 +52,14 @@ set -gx CPPFLAGS "-I/usr/local/opt/openssl@1.1/include"
 set -gx PKG_CONFIG_PATH "/usr/local/opt/openssl@1.1/lib/pkgconfig"
 
 # ********************************
+# ********* openblas env *********
+# ********************************
+
+set -gx LDFLAGS "-L/usr/local/opt/openblas/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/openblas/include"
+set -gx PKG_CONFIG_PATH "/usr/local/opt/openblas/lib/pkgconfig"
+
+# ********************************
 # ********** path env ************
 # ********************************
 
