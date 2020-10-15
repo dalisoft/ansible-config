a7d26ac004ed6db5f773f7782f0c7a137dd9b3d7
dalisoft <dalisoft@mail.ru>
2020-10-16 01:36:36 +0500

    fix: bug-fixes and adding new app(s)

--- run-ansible.sh
+++ run-ansible.sh
@@ -15,4 +15,4 @@ fi
 
 cd ansible
 ansible-galaxy install -r requirements.yml
-ansible-playbook -v --connection=local --become-user=$USER --extra-vars=$extra_vars --inventory 127.0.0.1, site.yml
+ansible-playbook -v --connection=local --become-user=$USER --extra-vars=$extra_vars site.yml
