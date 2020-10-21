c61bda357dcf88025a5a4c54a03d56f3a8a99a51
dalisoft <dalisoft@mail.ru>
2020-10-21 18:00:36 +0500

    initial version of new installation structure: mac common ver working

--- run-ansible.sh
+++ run-ansible.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-extra_vars=''
-install_mode='DESKTOP'
-
-if [ "$2" == "SERVER" ]
-then
-  install_mode=$2
-fi
-
-if [ "$1" != "" ]
-then
-  extra_vars="{\"SUDO_PASS\":\"$1\",\"ansible_become_pass\":\"$1\",\"USER\":\"$USER\",\"HOME\":\"$HOME\",\"INSTALL_MODE\":\"$install_mode\"}"
-fi
-
 cd ansible
-ansible-galaxy install -r requirements.yml
 ansible-playbook -v --connection=local --become-user=$USER --extra-vars=$extra_vars site.yml
