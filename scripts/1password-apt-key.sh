a1fb4ade4597911ef564ce528fe925e5b8b5cfef
dalisoft <dalisoft@mail.ru>
2020-10-23 20:27:20 +0500

    feat: add keyring installation

--- scripts/1password-apt-key.sh
+++
@@ -1,5 +0,0 @@
-echo "Requires sudo for properly adding!"
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
-
-echo "Added 1Password key into system"
