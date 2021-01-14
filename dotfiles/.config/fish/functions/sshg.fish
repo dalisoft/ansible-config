da5be58346aacb00d75029b23c6a0ae9f4f291d9
dalisoft <dalisoft@mail.ru>
2021-01-14 12:33:11 +0500

    fixes and improvements

--- dotfiles/.config/fish/functions/sshg.fish
+++ dotfiles/.config/fish/functions/sshg.fish
@@ -2,14 +2,19 @@ function sshg --argument ssh_path email power
     if count $power > /dev/null
         # Thanks
     else
-        $power = 100
+        set power 100
+    end
 
     if count $email > /dev/null
         # Thanks
     else
-        $email = "dalisoft@mail.ru"
-
+        set email "dalisoft@mail.ru"
+    end
+   
     if count $ssh_path > /dev/null
         ssh-keygen -o -a $power -t ed25519 -C $email -f $ssh_path
     else
         ssh-keygen -o -a $power -t ed25519 -C $email
+    end
+
+end
