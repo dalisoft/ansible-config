a7f8fd0b287c577890fdecfcab7957ead7d6fc2a
dalisoft <dalisoft@mail.ru>
2021-01-23 16:46:27 +0500

    fix(win): powershell script fix

--- ansible/common/os/windows/install.ps1
+++ ansible/common/os/windows/install.ps1
@@ -7,13 +7,12 @@ echo "Disabling prompt for every installation..."
 choco feature enable -n=allowGlobalConfirmation
 
 echo "Installing packages..."
-:repeat
-choco install firacode starship || goto :repeat              # Terminal essentials
-choco install git wget curl bat cmake || goto :repeat        # Utilities
-choco install python3 golang nodejs yarn || goto :repeat     # Languages essentials
-choco install rust-analyzer gradle anaconda3 || goto :repeat # Additions to PL
+choco install firacode starship              # Terminal essentials
+choco install git wget curl bat cmake        # Utilities
+choco install python3 golang nodejs yarn     # Languages essentials
+choco install rust-analyzer gradle anaconda3 # Additions to PL
 choco install heroku-cli                                     # Terminal additions
-choco install linkshellextension || goto :repeat             # Essential utilities
-choco install neovim || goto :repeat                         # Editor
+choco install linkshellextension             # Essential utilities
+choco install neovim                         # Editor
 
 echo "Done!"
