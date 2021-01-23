05bd6c6f36dd256401246390b19214e9c329f3dd
dalisoft <dalisoft@mail.ru>
2021-01-23 16:13:01 +0500

    feat: add initial experimental windows support

---
+++ ansible/common/os/windows/list.bash
@@ -0,0 +1,21 @@
+choco install firacode starship              # Terminal essentials
+choco install git wget curl bat cmake        # Utilities
+choco install python3 golang nodejs yarn     # Languages essentials
+choco install rust-analyzer gradle anaconda3 # Additions to PL
+choco install heroku-cli                     # Terminal additions
+
+choco install linkshellextension   # Essential utilities
+choco install 1password telegram   # Essential apps
+choco install googlechrome firefox # Browsers
+choco install vlc potplayer        # Media
+choco install neovim sublimetext3 vscode \
+  webstorm pycharm androidstudio        # Editors / IDE
+choco install postman                   # REST Client
+choco install octave                    # Advanced stuffs
+choco install steam epicgameslauncher   # Gaming store
+choco install vmware-workstation-player # VM
+
+choco install samsung-nvme-driver # Drivers
+
+choco install samsung-magician           # Required to install manually
+choco install nextdns --ignore-checksums # Broken packages
