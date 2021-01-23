ef9c264b6f96d9a3cb817073796be6e1e4cb31f7
dalisoft <dalisoft@mail.ru>
2021-01-23 16:43:46 +0500

    fix: use powershell for windows installation

--- ansible/common/os/windows/install.ps1
+++ ansible/common/os/windows/install.ps1
@@ -1,3 +1,7 @@
+# Install chocolately
+echo "Install chocolately..."
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 # First disable prompt
 echo "Disabling prompt for every installation..."
 choco feature enable -n=allowGlobalConfirmation
