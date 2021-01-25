4e80dc13239580f74983e5d2633d69e767a3ab33
dalisoft <dalisoft@mail.ru>
2021-01-25 18:48:54 +0500

    fix: fish global vars

--- dotfiles/.config/fish/paths/ssh-load.fish
+++ dotfiles/.config/fish/paths/ssh-load.fish
@@ -1,7 +1,7 @@
 # config.fish
 if not pgrep -f ssh-agent > /dev/null
   eval (ssh-agent -c)
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -gx SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  set -gx SSH_AGENT_PID $SSH_AGENT_PID
+  set -gx SSH_AUTH_SOCK $SSH_AUTH_SOCK
 end
