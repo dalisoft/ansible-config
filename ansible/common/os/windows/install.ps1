b5aa132ec106c3bcc03f00d8da9c596897e1f8d9
dalisoft <dalisoft@mail.ru>
2021-01-25 08:51:28 +0500

    fix: add common packages for windows

--- ansible/common/os/windows/install.ps1
+++ ansible/common/os/windows/install.ps1
@@ -1,25 +1,30 @@
-# Install chocolately
-echo "Installing chocolately..."
+# Install Chocolatey
+Write-Output "Installing chocolatey..."
 $testchoco = powershell choco -v
-if(-not($testchoco)){
-    echo "Seems Chocolatey is not installed, installing now"
-    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+if (-not($testchoco)) {
+	Write-Output "Seems Chocolatey is not installed, installing now"
+	Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
-else{
-    echo "Chocolatey Version $testchoco is already installed"
+else {
+	Write-Output "Chocolatey Version $testchoco is already installed"
 }
 
 # First disable prompt
-echo "Disabling prompt for every installation..."
+Write-Output "Disabling prompt for every installation..."
 choco feature enable -n=allowGlobalConfirmation
 
-echo "Installing packages..."
-choco install firacode starship              # Terminal essentials
-choco install git wget curl bat cmake        # Utilities
-choco install python3 golang nodejs yarn     # Languages essentials
-choco install rust-analyzer gradle anaconda3 # Additions to PL
-choco install heroku-cli                                     # Terminal additions
-choco install linkshellextension             # Essential utilities
-choco install neovim                         # Editor
+Write-Output "Installing packages..."
+choco install firacode starship bat ripgrep           # Terminal essentials
+choco install nano neovim                             # Terminal editors
+choco install git git-lfs git-cola \                  # Git
+			rsync rclone \                                  # Sync and Clone
+			curl wget \                                     # Fetching
+			gpg4win-vanilla unzip \                         # Utilities
+		  podman-machine podman-cli \                     # Podman
+		  ffmpeg ghostscript                              # Addinitional utilities
+choco install cmake python3 golang nodejs yarn deno   # Languages essentials
+choco install rust-analyzer gradle anaconda3          # Additions to PL
+choco install heroku-cli                              # Terminal additions
+choco install linkshellextension                      # Essential utilities
 
-echo "Done!"
+Write-Output "Done!"
