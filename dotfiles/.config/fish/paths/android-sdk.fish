3d3fcbfbad39b8198bfb5caf859b8fae1e2b0f55
dalisoft <dalisoft@mail.ru>
2021-02-18 14:00:31 +0500

    update config

--- dotfiles/.config/fish/paths/android-sdk.fish
+++ dotfiles/.config/fish/paths/android-sdk.fish
@@ -1,7 +1,10 @@
 # **** Android SDK ****
 set -gx ANDROID_HOME "$HOME/Library/Android/sdk"
+set -gx JAVA_HOME (/usr/libexec/java_home -v11)
 
 set -gx PATH "$ANDROID_HOME/emulator" $PATH
 set -gx PATH "$ANDROID_HOME/tools" $PATH
 set -gx PATH "$ANDROID_HOME/tools/bin" $PATH
 set -gx PATH "$ANDROID_HOME/platform-tools" $PATH
+
+set -gx PATH "$JAVA_HOME/bin" $PATH
