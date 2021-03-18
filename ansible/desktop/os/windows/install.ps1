9ed2a14db0ef8aa14c0c52caf9421981d36b5e0f
dalisoft <dalisoft@mail.ru>
2021-03-18 19:35:14 +0500

    fix: update apps list

--- ansible/desktop/os/windows/install.ps1
+++ ansible/desktop/os/windows/install.ps1
@@ -14,21 +14,22 @@ Write-Output "Disabling prompt for every installation..."
 choco feature enable -n=allowGlobalConfirmation
 
 Write-Output "Installing packages..."
-choco install pyqt heroku-cli kdiff3   # Additional tools
-choco install 7zip                     # Essential desktop tools
-choco install anaconda3 rust-analyzer  # Additional PL tools
-choco install obs vlc potplayer        # Media
-choco install anydesk                  # Remote management
-choco install googlechrome firefox     # Browsers
-choco install 1password telegram       # GUI Essential Apps
-choco install octave gitkraken postman # Development tools
-choco install sublimetext3 vscode      # Editors
-choco install android-studio           # IDE
+choco install pyqt heroku-cli kdiff3          # Additional tools
+choco install 7zip                            # Essential desktop tools
+choco install anaconda3 rust-analyzer         # Additional PL tools
+choco install obs vlc potplayer               # Media
+choco install freedownloadmanager qbittorrent # Download tools
+choco install anydesk                         # Remote management
+choco install googlechrome firefox            # Browsers
+choco install 1password telegram              # GUI Essential Apps
+choco install octave gitkraken postman datagrip # Development tools
+choco install sublimetext3 vscode             # Editors
+choco install android-studio                  # IDE
 
-choco install anacondajdk15            # Additional tools
-choco install vmware-workstation-player # VM
+choco install anacondajdk15                   # Additional tools
+choco install vmware-workstation-player       # VM
 
-choco install steam epicgameslauncher   # Gaming stores
+choco install steam epicgameslauncher         # Gaming stores
 
 choco install nextdns --ignore-checksum # Disable checksum until it got fixed
 choco install samsung-nvme-driver samsung-magician # Samsung Drivers / Tools (last should be installed manually)
