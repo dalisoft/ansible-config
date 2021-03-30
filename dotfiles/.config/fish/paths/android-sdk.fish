09dba64b9d8903d4e339fcc3e3b3410f642a1d51
dalisoft <dalisoft@mail.ru>
2021-03-30 23:26:58 +0500

    fix: android root fix

--- dotfiles/.config/fish/paths/android-sdk.fish
+++ dotfiles/.config/fish/paths/android-sdk.fish
@@ -1,5 +1,7 @@
 # **** Android SDK ****
 set -gx ANDROID_HOME "$HOME/Library/Android/sdk"
+set -gx ANDROID_SDK_ROOT "$HOME/Library/Android/sdk"
+set -gx ANDROID_AVD_HOME "$HOME/.android/avd"
 set -gx JAVA_HOME (/usr/libexec/java_home -v11)
 
 set -gx PATH "$ANDROID_HOME/emulator" $PATH
