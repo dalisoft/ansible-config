dd48f1017733f4b6898a9e0b204df61597c53666
dalisoft <dalisoft@mail.ru>
2021-04-11 13:47:17 +0500

    fix: port for m1 fix

--- dotfiles/.config/fish/paths/brew.fish
+++ dotfiles/.config/fish/paths/brew.fish
@@ -1,5 +1,6 @@
 # **** Brew ****
 set -gx HOMEBREW_NO_ANALYTICS 1
 set -gx PATH "/usr/local/bin" $PATH
+set -gx PATH "/opt/homebrew/bin" $PATH
 set -gx PATH "$HOMEBREW_PREFIX/bin" $PATH
 set -gx PATH "$HOMEBREW_PREFIX/sbin" $PATH
