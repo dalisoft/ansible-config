907b24f17c9cf23c097fc298949b1bce1ca9a014
dalisoft <dalisoft@mail.ru>
2021-04-29 14:23:46 +0500

    fix: remove postman

--- ansible/desktop/os/windows/install.ps1
+++ ansible/desktop/os/windows/install.ps1
@@ -22,7 +22,7 @@ choco install freedownloadmanager qbittorrent # Download tools
 choco install anydesk clockify                # Remote management
 choco install googlechrome firefox            # Browsers
 choco install 1password telegram slack        # GUI Essential Apps
-choco install octave gitkraken postman datagrip # Development tools
+choco install octave gitkraken datagrip       # Development tools
 choco install notion                          # Productivity
 choco install vscode                          # Editors
 choco install android-studio                  # IDE
