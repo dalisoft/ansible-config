9bd74a111390c046324debee529343bb1e30bf9a
dalisoft <dalisoft@mail.ru>
2021-04-29 14:33:20 +0500

    fix: update apps list on windows

--- ansible/desktop/os/windows/install.ps1
+++ ansible/desktop/os/windows/install.ps1
@@ -14,27 +14,27 @@ Write-Output "Disabling prompt for every installation..."
 choco feature enable -n=allowGlobalConfirmation
 
 Write-Output "Installing packages..."
-choco install pyqt heroku-cli kdiff3          # Additional tools
-choco install 7zip                            # Essential desktop tools
-choco install anaconda3 rust-analyzer         # Additional PL tools
-choco install obs vlc potplayer               # Media
-choco install freedownloadmanager qbittorrent # Download tools
-choco install anydesk clockify                # Remote management
-choco install googlechrome firefox            # Browsers
-choco install 1password telegram slack        # GUI Essential Apps
-choco install octave gitkraken datagrip       # Development tools
-choco install office2019proplus notion        # Productivity
-choco install vscode                          # Editors
-choco install android-studio                  # IDE
-choco install watchman                        # Dependencies
+choco install pyqt heroku-cli kdiff3             # Additional tools
+choco install 7zip                               # Essential desktop tools
+choco install anaconda3 rust-analyzer            # Additional PL tools
+choco install obs vlc potplayer                  # Media
+choco install internet-download-manager utorrent # Download tools
+choco install anydesk clockify                   # Remote management
+choco install googlechrome firefox               # Browsers
+choco install 1password telegram slack           # GUI Essential Apps
+choco install octave gitkraken datagrip          # Development tools
+choco install office2019proplus notion           # Productivity
+choco install vscode                             # Editors
+choco install webstorm android-studio            # IDE
+choco install watchman                           # Dependencies
 
-choco install anacondajdk15                   # Additional tools
-choco install vmware-workstation-player       # VM
+choco install anacondajdk15                      # Additional tools
+choco install vmware-workstation-player          # VM
 
-choco install steam epicgameslauncher         # Gaming stores
+choco install steam epicgameslauncher            # Gaming stores
 
-choco install warp                            # VPN
-choco install nextdns --ignore-checksum # Disable checksum until it got fixed
+choco install warp                               # VPN
+choco install nextdns --ignore-checksum          # Disable checksum until it got fixed
 choco install samsung-nvme-driver samsung-magician # Samsung Drivers / Tools (last should be installed manually)
 
 Write-Output "Done!"
