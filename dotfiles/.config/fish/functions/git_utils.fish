c5f059926b363153f5af0d724448703977ab1d0d
Davlatjon Shavkatov <opensource@dalisoft.uz>
2021-06-21 15:49:25 +0500

    update config
    Signed-off-by: Davlatjon Shavkatov <opensource@dalisoft.uz>

--- dotfiles/.config/fish/functions/git_utils.fish
+++ dotfiles/.config/fish/functions/git_utils.fish
@@ -1,31 +1,6 @@
-function gitstat
-    git status --show-stash -b -uall --renames
-end
-
-function gitre
-    git reflog expire --expire=now --all
-end
-
-function gitra
-    git repack -ad
-end
-
-function gitp
-    git prune
-end
-
 function gitp_branch
     set branch (git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@");
     git branch --merged $branch | grep -v "^[ *]*$branch" | xargs git branch -d
-end
-
-
-function gitf
-    git fetch --all --prune
-end
-
-function gitsi
-    git submodule update --init --recursive --remote --rebase
 end
 
 function gitsu
@@ -36,10 +11,4 @@ function gitsu
     git checkout ${branch}; \
     git reset origin/${branch} --soft;\
     ';
-end
-
-function git_rb
-    gitf
-    git pull --rebase
-    gitsu
 end
