2d62fea9a4938eac95f9f83817af61f4aa695b01
Davlatjon Shavkatov <opensource@dalisoft.uz>
2021-06-22 21:29:50 +0500

    fix(bs): update script for more better solution

--- machine/mac/intel/install.sh
+++ machine/mac/intel/install.sh
@@ -4,65 +4,97 @@ set -e
 PASSWORD=$1
 MODE=$2
 
-if [[ "$PASSWORD" == "" ]]; then
-  echo "Hey, welcome! please trust me"
-  echo "and enter valid password here"
-  echo "I hope you understand me..."
-  exit 1
-fi
-if [[ "$MODE" != "minimal" && $MODE != "compact" && $MODE != "full" ]]; then
-  echo "Hey, welcome!"
-  echo "Please select any of these values"
-  echo "minimal, compact, full"
-  exit 1
-fi
+function check_env {
+  if [[ "$PASSWORD" == "" ]]; then
+    echo "Hey, welcome! please trust me"
+    echo "and enter valid password here"
+    echo "I hope you understand me..."
+    exit 1
+  fi
+  if [[ "$MODE" != "minimal" && $MODE != "compact" && $MODE != "full" ]]; then
+    echo "Hey, welcome!"
+    echo "Please select any of these values"
+    echo "minimal, compact, full"
+    exit 1
+  fi
+}
 
-# Configure askpass
-rm -rf askpass.sh
-echo "#!/bin/sh" >>./askpass.sh
-echo "echo \"$PASSWORD\"" >>./askpass.sh
-chmod 700 askpass.sh
-export SUDO_ASKPASS=$(pwd)/askpass.sh
+function configure_askpass {
+  rm -rf askpass.sh
+  echo "#!/bin/sh" >>./askpass.sh
+  echo "echo \"$PASSWORD\"" >>./askpass.sh
+  chmod 700 askpass.sh
+  export SUDO_ASKPASS=$(pwd)/askpass.sh
+}
 
-if [[ "$EUID" = 0 ]]; then
-  echo "Hey, welcome! I got (sudo) access"
-  echo "Thank you for your trust, so"
-  echo "i will continue my processes"
-else
-  if sudo -A true; then
-    echo "Hey, welcome! I got valid password"
+function sudo_access_check {
+  if [[ "$EUID" = 0 ]]; then
+    echo "Hey, welcome! I got (sudo) access"
     echo "Thank you for your trust, so"
     echo "i will continue my processes"
   else
-    echo "Hey, how are you?"
-    echo "Seems password is not valid"
-    echo "Please check enter again..."
-    echo "Thank you"
-    rm -rf askpass.sh
-    exit 1
+    if sudo -A true; then
+      echo "Hey, welcome! I got valid password"
+      echo "Thank you for your trust, so"
+      echo "i will continue my processes"
+    else
+      echo "Hey, how are you?"
+      echo "Seems password is not valid"
+      echo "Please check enter again..."
+      echo "Thank you"
+      rm -rf askpass.sh
+      exit 1
+    fi
   fi
-fi
+}
 
-if sudo -A xcode-select --version >>/dev/null; then
-  echo "XCode already installed! Continue process..."
-else
-  sudo -A xcode-select --install
-fi
+function check_and_prepare {
+  check_env
+  configure_askpass
+  sudo_access_check
+}
 
-# Install Homebrew
-if brew --version >>/dev/null; then
-  echo "Homebrew already installed! Continue process..."
-else
-  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -
-fi
+function installation {
 
-# Installing bundle
-cd $MODE
-brew bundle
+  if sudo -A xcode-select --version >>/dev/null; then
+    echo "XCode already installed! Continue process..."
+  else
+    sudo -A xcode-select --install
+  fi
 
-# Remove password by removing askpass
-rm -rf ../askpass.sh
+  # Install Homebrew
+  if brew --version >>/dev/null; then
+    echo "Homebrew already installed! Continue process..."
+  else
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -
+  fi
 
-echo "Your apps installed successfully..."
-echo "Enjoy..."
-exit 0
+  # Installing bundle
+  cd $MODE
+  brew bundle
+
+  # Remove password by removing askpass
+  rm -rf ../askpass.sh
+
+  return 0
+}
+
+RETRIES=0
+MAX_TRIES=5
+
+# Run
+check_and_prepare
+
+while true; do
+  if installation; then
+    echo "Your apps installed successfully..."
+    echo "Enjoy..."
+    break
+    exit 0
+  else
+    echo "Something got wrong..."
+    echo "Retrying $RETRIES of $MAX_TRIES"
+    RETRIES=$((RETRIES + 1))
+    sleep 15
+  fi
+done
