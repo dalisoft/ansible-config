d97dbfa3bd307131784dd7b43763dae0eb1fe7ec
Davlatjon Shavkatov <git@dalisoft.uz>
2021-07-28 04:41:18 +0500

    fix: add `git_correct_tags`

--- dotfiles/.config/fish/functions/git_utils.fish
+++ dotfiles/.config/fish/functions/git_utils.fish
@@ -53,5 +53,27 @@ function git_sign_tags
   git push --tags --force --no-verify
 end
 
+function git_correct_tags
+  git tag -l | while read -l tag
+    set TAG_COMMIT_HASH (git rev-list -1 $tag)
+    set TAG_COMMIT_RAW_MSG (git tag -l --format='%(contents)' $tag | head -n1)
+    set TAG_COMMIT_SIGN (echo $TAG_COMMIT_RAW_MSG | grep -o -- 'Signed-off-by(.*)')
+    if count $TAG_COMMIT_SIGN >> /dev/null
+      set TAG_COMMIT_MSG (echo $TAG_COMMIT_RAW_MSG | sed "s/$TAG_COMMIT_SIGN//g")
+    else
+      set TAG_COMMIT_MSG $TAG_COMMIT_RAW_MSG
+    end
+
+    set SEARCH_COMMIT_HASH (git log --fixed-strings --grep "$TAG_COMMIT_MSG" --no-decorate --format=%H --max-count=1)
+    set SEARCH_COMMIT_MSG (git show $SEARCH_COMMIT_HASH -q --format=%B)
+    set GIT_COMMITTER_DATE "(git show $SEARCH_COMMIT_HASH --format=%aD | head -1)"
+
+    if begin count $SEARCH_COMMIT_HASH >> /dev/null; and test "$TAG_COMMIT_HASH" != "$SEARCH_COMMIT_HASH"; end
+      git tag -s -a -f $tag -m"$SEARCH_COMMIT_MSG" $SEARCH_COMMIT_HASH
+      git push origin refs/tags/$tag --force --no-verify
+    end
+  end
+end
+
 alias git_rebase='git rebase -S --committer-date-is-author-date'
 alias gitsuf='git submodule update --init --recursive --remote --force --checkout'
