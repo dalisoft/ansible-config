4284ef03bd59c9d78c96d435e29148b27bdbd042
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-08-18 23:11:36 +0500

    fix(mac-config-bash::utils): fix `metadata` for mkv2mp4

--- mac-config-bash/utils/mkv2mp4.sh
+++ mac-config-bash/utils/mkv2mp4.sh
@@ -4,7 +4,7 @@ set -e
 if [ "${1: -4}" == ".mkv" ]; then
   filename="${1%.*}"
   echo "Found mkv file, starting convertation"
-  ffmpeg -y -i "$1" -map 0:v -map 0:a -map 0:s -c copy -c:s mov_text -strict unofficial "$filename.mp4"
+  ffmpeg -y -i "$1" -movflags use_metadata_tags -map 0:v -map 0:a -map 0:s -c copy -c:s mov_text -strict unofficial "$filename.mp4"
   echo "Done"
 else
   echo "No MKV file found"
